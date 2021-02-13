1982d073f4a1268fcc38577a44082899ce2597b0
Jason Overby <j@jasonoverby.com>
2021-02-13 12:56:46 -0800

    add UpdateImage component

--- components/ChangeImage.jsx
+++ components/ChangeImage.jsx
@@ -1,58 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { updateImage, getNewImageIndex } from '/lib/utils';
+import { updateImage, getNewImageIndex } from '../lib/utils';
+
+export const UpdateImage = ({
+  incrementOrDecrement,
+  imageIndex,
+  setActiveDotIndex,
+  setImageIndex,
+  setImageLoaded,
+}) => (
+  <button
+    aria-label="show previous image"
+    className="changeImage"
+    onClick={async () => {
+      const updatedImageIndex = getNewImageIndex(
+        imageIndex,
+        incrementOrDecrement,
+      );
+      await updateImage({
+        setActiveDotIndex,
+        setImageIndex,
+        setImageLoaded,
+        updatedImageIndex,
+      });
+    }}
+    type="button"
+  >
+    <span className="arrow">
+      {incrementOrDecrement === 'decrement' ? '◀' : '▶'}
+    </span>
+  </button>
+);
 
 export const DecrementImage = ({
   imageIndex,
   setActiveDotIndex,
   setImageIndex,
   setImageLoaded,
-}) => (
-  <button
-    aria-label="show previous image"
-    className="changeImage"
-    onClick={async () => {
-      const updatedImageIndex = getNewImageIndex(imageIndex, 'decrement');
-      await updateImage({
-        setActiveDotIndex,
-        setImageIndex,
-        setImageLoaded,
-        updatedImageIndex,
-      });
-    }}
-  >
-    <span className="arrow">◀</span>
-  </button>
-);
+}) =>
+  UpdateImage({
+    imageIndex,
+    incrementOrDecrement: 'decrement',
+    setActiveDotIndex,
+    setImageIndex,
+    setImageLoaded,
+  });
 
 export const IncrementImage = ({
   imageIndex,
   setActiveDotIndex,
   setImageIndex,
   setImageLoaded,
-}) => (
-  <button
-    aria-label="show previous image"
-    className="changeImage"
-    onClick={async () => {
-      const updatedImageIndex = getNewImageIndex(imageIndex, 'increment');
-      await updateImage({
-        setActiveDotIndex,
-        setImageIndex,
-        setImageLoaded,
-        updatedImageIndex,
-      });
-    }}
-  >
-    <span className="arrow">▶</span>
-  </button>
-);
+}) =>
+  UpdateImage({
+    imageIndex,
+    incrementOrDecrement: 'increment',
+    setActiveDotIndex,
+    setImageIndex,
+    setImageLoaded,
+  });
 
 DecrementImage.propTypes = {
-  imageIndex: PropTypes.number,
-  setActiveDotIndex: PropTypes.func,
-  setImageIndex: PropTypes.func,
-  setImageLoaded: PropTypes.func,
+  imageIndex: PropTypes.number.isRequired,
+  setActiveDotIndex: PropTypes.func.isRequired,
+  setImageIndex: PropTypes.func.isRequired,
+  setImageLoaded: PropTypes.func.isRequired,
 };
 IncrementImage.propTypes = DecrementImage.propTypes;
+UpdateImage.propTypes = {
+  ...DecrementImage.propTypes,
+  incrementOrDecrement: PropTypes.string.isRequired,
+};
